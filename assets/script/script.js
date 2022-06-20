cf540f70c03bbed401dd49808c3c398da52da8c5
Andrea Bell <andreabell@Andreas-iMac.local>
2022-06-20 10:42:58 +1000

    js

---
+++ assets/script/script.js
@@ -0,0 +1,53 @@
+// current date using moment
+$("#currentDay").text(moment().format("dddd") + ", " + moment().format("MMMM Do") + ", " + moment().format("YYYY"));
+
+//Start Document
+$(document).ready(function(){
+    // Functions called
+    assessHour();
+    localUser();
+});
+
+// Function for if time slot is past, present or future
+// Colour of fields changed based on this criteria
+function assessHour() {
+
+    // Declare variables to parse for integer
+    var currentTime = moment().format("HH");
+    var timeInt = parseInt(currentTime);
+    // IF/ELSE statements inside FOR loop to assess standard business hours (9-5)
+    for(var i = 9; i < 18; i++) {
+        
+        // Declare Variables to parse for integer
+        var timeSlot = $("#" + i).attr("data-index");
+        var timeSlotInt= parseInt(timeSlot);
+
+
+        // TimeSlot assessment
+        if(timeInt > timeSlotInt){
+            $("#" + i).addClass("past");
+        } if(timeInt == timeSlotInt) {
+            $("#" + i).addClass("present");
+        } if(timeInt < timeSlotInt) {
+            $("#" + i).addClass("future");
+        }
+    }
+}
+
+// Local storage function
+function localUser(){
+    //Store locally create loop
+    for(var i = 9; i < 18; i++) {
+        $("#" + i).val(localStorage.getItem(i));
+    }
+}
+
+// To save field input function for users
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function() {
+    var pTime = $(this).attr("data-index");
+    console.log(pTime);
+    var userInput = $("#" + pTime).val();
+    console.log(userInput);
+    localStorage.setItem(pTime, userInput);
+});
